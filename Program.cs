9fa3b4c596fa066159d65dae13fbf7a66ceb118b
rishabhshi2001 <rishabhshishodia11@gmail.com>
2025-09-02 22:41:58 +0530

    Read File and Delete Later

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace MyCode
     {
         static void Main(string[] args)
         {
-            Console.Write("I am Rishabh Shishodia");
+            string filePath = @"C:\Users\91858\Dropbox\PC\Desktop\License.txt";
+            string content = "Active Directory<B>5000 \n File server<B>4000 \n EMC<B>3000 \n SQL Server<B>5000";
+            File.WriteAllText(filePath, content);
+            long User = 0;
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    User += GetData(line);
+                }
+            }
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+            Console.Write(User);
+        }
+
+        static int GetData(string content)
+        {
+            int start = content.IndexOf("<B>") + 3;
+            string numberString = content.Substring(start);
+            int value = int.Parse(numberString);
+            return value;
         }
     }
 }
